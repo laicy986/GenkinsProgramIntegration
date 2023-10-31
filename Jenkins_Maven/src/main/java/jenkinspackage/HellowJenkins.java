b1513adf75a2531dcb244a3a5e855f38a835841f
laicy <laicythomas@gmail.com>
2023-10-31 19:36:45 +0100

    Jenkins project moved

---
+++ Jenkins_Maven/src/main/java/jenkinspackage/HellowJenkins.java
@@ -0,0 +1,11 @@
+package jenkinspackage;
+
+public class HellowJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello");
+	}
+
+}
